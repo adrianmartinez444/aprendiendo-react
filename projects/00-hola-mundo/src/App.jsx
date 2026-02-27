90e746bfdc3509cf2600244eb6b6d2e39e435d1f
adrianmartinez444 <adrian.martinez.rey.0@gmail.com>
2026-02-27 16:21:30 -0500

    Se pasaron las funciones como props

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
-  const formatUsername = (userName) => `@${userName}`
+  const format = (userName) => `@${userName}`
   return (
     <section className="App">
-      <TwitterFollowCard formatUsername={formatUsername} isFollowing={false} userName = "midudev" name = "Adrian Martinez" />
-      <TwitterFollowCard formatUsername={formatUsername} isFollowing={true} userName = "pheralb" name = "Pablo Hernandez" />
-      <TwitterFollowCard formatUsername={formatUsername}  userName = "adrianmartinez444" name = "Adrian Martinez" />
+      <TwitterFollowCard formatUsername={format} isFollowing={false} userName = "midudev" name = "Adrian Martinez" />
+      <TwitterFollowCard formatUsername={format} isFollowing={true} userName = "pheralb" name = "Pablo Hernandez" />
+      <TwitterFollowCard formatUsername={format}  userName = "adrianmartinez444" name = "Adrian Martinez" />
     </section>
   )
 }
